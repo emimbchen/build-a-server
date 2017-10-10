acea7a3a921061cf940d4207496aafcd4f7bedf8
Emi Chen <emimbchen@gmail.com>
2017-10-10 16:27:10 -0500

    fixed rogue quotes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var port = 5000;
 app.use(express.static('server/public'));
-app.listen('port', function(){
+app.listen(port, function(){
 console.log('listening on port', port);
 });
